12974f4af0ced32b951e7668646fdcb7c64ff553
Bazingator <60516660+Bazingator@users.noreply.github.com>
2022-08-10 10:31:33 -0400

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -4,18 +4,22 @@ public class Demo {
 		CeilingFan fan = new CeilingFan();
 		System.out.println("Ceiling fan created.");
 		System.out.println(fan.getStatus());
+		
 		System.out.println("Pulling speed and direction cord once.");
 		fan.pullDirectionCord();
 		fan.pullSpeedCord();
 		System.out.println(fan.getStatus());
+		
 		System.out.println("Pulling speed cord twice.");
 		fan.pullSpeedCord();
 		fan.pullSpeedCord();
 		System.out.println(fan.getStatus());
+		
 		System.out.println("Pulling speed and direction cord once.");
 		fan.pullDirectionCord();
 		fan.pullSpeedCord();
 		System.out.println(fan.getStatus());
+		
 		System.out.println("Pulling speed cord 4 times and direction cord once.");
 		fan.pullDirectionCord();
 		fan.pullSpeedCord();
